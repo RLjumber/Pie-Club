74d4a398232b4dedb621f4533644f36b6ae1c8f3
RLjumber <ronaldjumber@gmail.com>
2023-05-19 22:49:19 -0400

    updated prisma models to include recipes

--- prisma/index.js
+++ prisma/index.js
@@ -2,31 +2,40 @@ const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
-async function main() {
-    await prisma.user.create({
+async function main(userId, recipeData) {
+  try {
+    const recipe = await prisma.recipe.create({
       data: {
-        name: 'Ali',
-        email: 'ali@pieclub.com',
-        posts: {
-          create: { title: 'Let me tell you about my pies' },
-        },
-        profile: {
-          create: { bio: 'I took a big fart in a pie once' },
+        title: recipeData.title,
+        content: recipeData.content,
+        ingredients: recipeData.ingredients,
+        steps: recipeData.steps,
+        author: {
+          connect: { id: userId },
         },
       },
     })
-  
-    const allUsers = await prisma.user.findMany({
-      include: {
-        posts: true,
-        profile: true,
-      },
-    })
-    console.dir(allUsers, { depth: null })
-  }
 
-main()
-  .then(async () => {
+    // console.log('Recipe created:', recipe)
+
+    return recipe
+  } catch (error) {
+    console.error('Error creating recipe:', error)
+    throw error
+  }
+}
+
+const userId = "2edcccf7-4aa2-48c4-8077-e300afd02d54"// Replace with the actual user ID
+const recipeData = {
+  title: 'Apple pie',
+  content: 'Its not your typical apple pie ;)',
+  ingredients: ['Apples?', 'Sugar', 'Other Pie stuff'],
+  steps: ['Put the apples on the floor', 'Slap your mother with the pie crust', 'Roll up into a ball and cry'],
+}
+
+main(userId, recipeData)
+  .then(async (recipe) => {
+    console.log('Recipe created:', recipe)
     await prisma.$disconnect()
   })
   .catch(async (e) => {
---
+++ prisma/migrations/20230520003724_recipes_model/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Recipe" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "ingredients" TEXT[],
+    "steps" TEXT[],
+    "authorId" UUID NOT NULL,
+
+    CONSTRAINT "Recipe_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Recipe" ADD CONSTRAINT "Recipe_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
